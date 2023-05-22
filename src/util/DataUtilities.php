deef52fe35e8b52bcaabcfd23af50fd5690a4ac9
Patrick Murray-John <p.murray-john@northeastern.edu>
2023-05-22 15:37:07 -0400

    add shortArrayExport
    provides DataUtil::shortArrayExport to support documenting renderArrays
    using short syntax
    
    close #81

--- src/util/DataUtilities.php
+++ src/util/DataUtilities.php
@@ -112,7 +112,30 @@ class DataUtilities {
 
     }
 
-
+    /**
+     * shortArrayExport
+     * 
+     * Does PHP's var_export for an array, but uses new, shorter syntax
+     *  
+     * Thanks https://gist.github.com/Bogdaan/ffa287f77568fcbb4cffa0082e954022
+     * 
+     * @param array $array  the array to export
+     * @param boolean $return whether to return the result or echo it directly. Default true
+     * @return string|void
+     */
+    
+    function shortArrayExport(array $array, bool $return = true) {
+        $export = var_export($array, TRUE);
+        $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
+        $array = preg_split("/\r\n|\n|\r/", $export);
+        $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
+        $export = join(PHP_EOL, array_filter(["["] + $array));
+        if ((bool)$return) {
+            return $export; 
+        } else {
+            echo $export;
+        }
+    }
 
     static function typeForOption($option, $scope = 'ceres') {
         self::setData();

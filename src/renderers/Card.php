721afbcaca9234b2594e7e6e24074de76d0cf62e
Patrick Murray-John <p.murray-john@northeastern.edu>
2023-05-23 15:03:34 -0400

    add Card renderer

---
+++ src/renderers/Card.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Ceres\Renderer;
+
+use Ceres\Renderer\Html;
+
+class Card extends HTML {
+
+    protected array $summaryRenderArray;
+    protected array $detailsRenderArray;
+
+
+    public function build() {
+        
+    }
+
+    
+
+
+
+}
+
+
+

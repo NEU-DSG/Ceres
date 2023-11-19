088738e641e2f1c56413f141c4db10403762614f
patrickmj <p.murray-john@northeastern.edu>
2023-11-19 15:01:22 -0500

    Add the mock data templates for deving BB
    need to fill some out now that (hopefully) I have the basic pattern
    understood

---
+++ data/testStoryMapStructure.js
@@ -0,0 +1,16 @@
+const storymaps = {
+    "id1": {
+        "label": "The storymap title/label/whatever",
+        "uris": [
+            "http://whatever",
+            "http://whatever2"
+        ]
+    },
+    "id2": {
+        "label": "The storymap title/label/whatever",
+        "uris": [
+            "http://whatever",
+            "http://whatever2"
+        ]
+    }
+}

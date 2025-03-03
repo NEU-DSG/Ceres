3a8062b2217a1cb6c6725ed4b7052508bafe9b54
patrickmj <p.murray-john@northeastern.edu>
2025-03-03 14:06:28 -0500

    remove setup and mockData from tracking
    close #111
    close #112

--- config/ceresJsSetup.js
+++
@@ -1,8 +0,0 @@
-
-/* a placeholder file to shove data from PHP into js
- * e.g. WP's `add_inline_script` https://developer.wordpress.org/reference/functions/wp_add_inline_script/
-
-*/
-
-
-//alert('setting up js');
